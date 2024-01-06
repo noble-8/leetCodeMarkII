5e70152a76bb212e01a69fb027f2ad99eba5e8c3
noble-8 <akash7nair@gmail.com>
2024-01-05 17:18:16 -0800

    majority element

--- 169-majority-element/sol.py
+++ 169-majority-element/sol.py
@@ -2,8 +2,7 @@ class Solution:
     def majorityElement(self, arr: List[int]) -> int:
         ans = arr[0]
         freq = 1
-        for i in range(len(arr)):
-            print(i)
+        for i in range(1,len(arr)):
             if arr[i]!=ans:
                 freq-=1
                 # print(freq)
---
+++ 169-majority-element/sol2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        ele = None
+        for i in range(len(nums)):
+            if count==0:
+                ele = nums[i]
+            if ele ==nums[i]:
+                count+=1
+            else:
+                count-=1
+        return ele
